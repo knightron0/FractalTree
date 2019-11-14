33b7865ea6986686e25cd02d5e4550635b6970ec
knightron0 <sarthak.robo@gmail.com>
2019-11-15 01:37:39 +0530

    start

---
+++ edge.py
@@ -0,0 +1,20 @@
+class edge():
+    def __init__(self, x, y, sz, cnt):
+        self.x = x
+        self.y = y
+        self.sz = sz
+        self.cnt = cnt
+        self.update
+        print("hello")
+    def update():
+        translate(x, y)
+        line(0, 0, 0, -sz)
+        # if cnt < 5:
+        #     translate(0, -sz)
+        #     rotate(a)
+        #     e1 = edge(0, 0, sz * (0.67), cnt+1)
+        #     e1.update()
+        #     rotate(-a)
+        #     rotate(-a)
+        #     e2 = edge(0, 0, sz + (0.67), cnt+1)
+        #     e2.update()
--- fractal_tree.pyde
+++ fractal_tree.pyde
@@ -1,1 +1,13 @@
+import edge as Edge
 
+def setup():
+    size(600, 600)
+    colorMode(HSB)
+    noStroke();
+    
+    
+def draw():
+    print("hello")
+    background(255)
+    root = Edge.edge(0, 0, 200, 0)
+    root.update
